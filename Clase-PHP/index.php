0c188b3dba693bfca7c37840398eeaa56a44300b
CamiloHdez97 <45055602+CamiloHdez97@users.noreply.github.com>
2023-05-18 09:32:59 -0500

    Add files via upload

--- Clase-PHP/index.php
+++ Clase-PHP/index.php
@@ -1,17 +1,117 @@
-<?php
-//echo '<h1>Hola Mundo Cruel</h1>';
-//$texto = "Mundo";
-//printf("Hola % , Cruel")
-$edad = 25;
-$nombre = "Camilo";
-echo var_dump($nombre);
-?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="container">
+    <form action="saveregistro.php" method="POST">
 
-<form action="saveregistro.php" method="POST">
-    CC: <input type="text" name="cc"><br>
-    Nombre: <input type="text" name="nombre"><br>
-    Email: <input type="text" name="email"><br>
-    Movíl: <input type="text" name="movil"><br>
-    <input type="submit" value="Enviar">
-</form>
+        <div class="form-row">
+            
+            <div class="form-group col-md-6">
+                <label for="inputEmail4">CC</label>
+                <input type="text" class="form-control" id="inputEmail4">
+            </div>
+            
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Nombre</label>
+                <input type="text" class="form-control" id="inputPassword4">
+            </div>
+    </div>
+
+        <div class="form-group">
+            <label for="inputAddress">Apellidos</label>
+            <input type="text" class="form-control" id="inputAddress">
+        </div>
+
+        <div class="form-group">
+            <label for="inputAddress2">Email</label>
+            <input type="email" class="form-control" id="inputAddress2">
+        </div>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputCity">Telefono</label>
+                <input type="text" class="form-control" id="inputCity">
+            </div>
+        </div>
+    <br>
+        <label>Estudios:</label>
+        <select name="titulo">
+            <option value="Bachiller">Bachiller</option>
+            <option value="Tecnico">Técnico</option>
+            <option value="Tecnologo">Tecnólogo</option>
+            <option value="Profesional">Profesional</option>
+            <option value="Estudiante">Estudiante</option>Universitario
+            <option value="Universitario">Universitario</option>
+        <select>
+        <br>
+
+
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="Python" name="lenguajes[]" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+            Python
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="CSS" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        CSS
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="JS" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        JS
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="PHP" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        PHP
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="Node JS" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        Node JS
+        </label>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="C#" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        C#
+        </label>
+    </div>
+
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="TypeScript" name="lenguajes[]" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">
+        TypeScript
+        </label>
+    </div>
+        <br>
+
+    <label>Nivel de Ingles:</label>
+    <select name="ingles">
+        <option value="Basico">Básico</option>
+        <option value="Intermedio">Intermedio</option>
+        <option value="Avanzado">Avanzado</option>
+    <select><br>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    <button type="reset" class="btn btn-primary">Reset</button>
+    </form>
+
+</div>
+    
+</body>
+</html>
 
--- Clase-PHP/saveregistro.php
+++ Clase-PHP/saveregistro.php
@@ -1,6 +1,39 @@
-Hola <?php isset($_POST["nombre"]) ? print $_POST["nombre"] : ""; ?><br>
+Hola -> <?php isset($_POST["nombre"]) ? print $_POST["nombre"] : ""; ?> <h1>Info de contacto: </h1> <br>
 
-<?php isset($_POST["cc"]) ? printf("CC: %s", $_POST["cc"]) : ""; ?><br>
-<?php isset($_POST["nombre"]) ? printf("Nombre: %s", $POST[""]) : ""; ?><br>
-<?php isset($_POST["email"]) ? printf("Email: %s", $_POST["email"]) : ""; ?><br>
-<?php isset($_POST["movil"]) ? printf("Movíl: %s", $_POST["movil"]) : ""; ?>
+<?php isset($_POST["cc"]) ? printf("CC: %s", $_POST["cc"]) : "";?><br>
+<?php isset($_POST["nombre"]) ? printf("Nombre: %s", $_POST["nombre"]) : "";?><br>
+<?php isset($_POST["email"]) ? printf("Email: %s", $_POST["email"]):"";?><br>
+<?php isset($_POST["movil"]) ? printf("Movíl: %s", $_POST["movil"]):"";?><br>
+
+
+<?php
+
+    $estudiante = [
+
+        'cc' => $_POST["cc"],
+        'nombre' => $_POST["nombre"],
+        'email' => $_POST["email"],
+        'movil' => $_POST["movil"],
+        'nivelEstudio' => $_POST["titulo"],
+        'lenguajes' => $_POST["lenguajes"],
+        'ingles' => $_POST["ingles"],
+
+    ];
+
+    $cantidad = count($_POST["lenguajes"]);
+    
+    if ($cantidad <= 2 && $_POST["ingles"] == "Basico"){
+        window.location.replace("http://aceptadoApolo.php");
+    }
+    else if ($cantidad <= 4 && $_POST["ingles"] == "Intermedio"){
+        window.location.replace("http://localhost:3000/aceptadoArtemis.php");
+    }
+        
+    else if ($cantidad <= 6 && $_POST["ingles"] == "Avanzado"){
+        window.location.replace("http://localhost:3000/aceptadoSputnik.php");
+    }
+
+    else {echo "No admitido";}
+
+?>
+
